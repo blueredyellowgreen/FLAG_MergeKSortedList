13cf392004f400b35728af0eb9a4c1b407b67299
blueredyellowgreen <68790601+blueredyellowgreen@users.noreply.github.com>
2020-08-02 19:35:12 -0400

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,9 @@
 class Solution {
     public static ListNode mergeKLists(ListNode[] lists) {
         // TODO: write your answer
+        
+        // Get started using Git!
+        
         return null;
     }
 }
